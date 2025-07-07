3a993a643f2e7fc18ed0fc8d5991b7c6d3fa7b62
fardanjamhal <fardanjamhal@gmail.com>
2025-07-07 16:09:38 +0800

    perbaharui3

--- admin/surat/permintaan_surat/index.php
+++ admin/surat/permintaan_surat/index.php
@@ -1,4 +1,10 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
+
+<?php
   include ('../../../config/koneksi.php');
   include ('../part/akses.php');
   include ('../part/header.php');
