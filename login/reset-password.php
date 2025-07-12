f2756f337b3c939d52d398a0965877eec5776433
fardanjamhal <fardanjamhal@gmail.com>
2025-07-12 13:29:11 +0800

    perbaharui3

--- login/reset-password.php
+++ login/reset-password.php
@@ -94,6 +94,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['token'])) {
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 	<style>
 	* {
