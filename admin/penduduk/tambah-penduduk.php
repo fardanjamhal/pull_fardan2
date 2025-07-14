cbaa382a8341c2023aa8ea09ad3e494c94f43e9d
fardanjamhal <fardanjamhal@gmail.com>
2025-07-14 19:55:17 +0800

    perbaharui3

--- admin/penduduk/tambah-penduduk.php
+++ admin/penduduk/tambah-penduduk.php
@@ -5,217 +5,148 @@
   include ('../../config/koneksi.php');
 ?> 
 
-
 <div class="content-wrapper">
+  <!-- HEADER -->
   <section class="content-header">
-    <h1>&nbsp;</h1>
+    <h1>Data Penduduk</h1>
     <ol class="breadcrumb">
       <li><a href="../dashboard/"><i class="fa fa-tachometer-alt"></i> Dashboard</a></li>
       <li class="active">Data Penduduk</li>
     </ol>
   </section>
+
+  <!-- CONTENT -->
   <section class="content">
     
-  <div class="col-md-12">
-  <a href="download-template.php" class="btn btn-success">
-    <i class="fa fa-download"></i> Download Template Excel
-  </a>
-  <br><br>
-  </div>
+    <!-- Tombol Download Template -->
+    <div class="row">
+      <div class="col-md-12 mb-3">
+        <a href="download-template.php" class="btn btn-success">
+          <i class="fa fa-download"></i> Download Template Excel
+        </a>
+      </div>
+    </div>
 
+    <!-- Form Import Excel -->
     <div class="row">
       <div class="col-md-12">
-        <form method="post" enctype="multipart/form-data" action="import-penduduk.php">
-          <div class="col-md-3">
-            <input name="datapenduduk" type="file" required="required">
+        <div class="box box-primary">
+          <div class="box-header with-border">
+            <h3 class="box-title"><i class="fa fa-upload"></i> Import Data Penduduk</h3>
           </div>
-          <div>
-            <input name="upload" type="submit" class="btn btn-primary" value="Import .XLS">
-          </div>
-        </form><br>
-      </div>
-        <div class="col-md-12">
-          <div class="box box-default">
-            <div class="box-header with-border">
-              <h3 class="box-title"><i class="fas fa-user-plus"></i> Tambah Data Penduduk</h3>
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
-              </div>
-            </div>
           <div class="box-body">
-            <div class="row">
-              <form class="form-horizontal" method="post" action="simpan-penduduk.php">
+            <form method="post" enctype="multipart/form-data" action="import-penduduk.php" class="form-inline">
+              <div class="form-group">
+                <input type="file" name="datapenduduk" class="form-control" required>
+              </div>
+              <button type="submit" class="btn btn-primary" style="margin-left: 10px;">
+                <i class="fa fa-file-import"></i> Import .XLS
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Form Tambah Data Penduduk -->
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box box-default">
+          <div class="box-header with-border">
+            <h3 class="box-title"><i class="fas fa-user-plus"></i> Tambah Data Penduduk</h3>
+            <div class="box-tools pull-right">
+              <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+              <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
+            </div>
+          </div>
+
+          <div class="box-body">
+            <form class="form-horizontal" method="post" action="simpan-penduduk.php">
+              <div class="row">
+                <!-- Kolom Kiri -->
                 <div class="col-md-6">
                   <div class="box-body">
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">NIK</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fnik" maxlength="16" onkeypress="return hanyaAngka(event)" class="form-control" placeholder="NIK" required>
-                        <script>
-                          function hanyaAngka(evt){
-                            var charCode = (evt.which) ? evt.which : event.keyCode
-                            if (charCode > 31 && (charCode < 48 || charCode > 57))
-                            return false;
-                            return true;
-                          }
-                        </script>
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Nama</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fnama" class="form-control" style="text-transform: capitalize;" placeholder="Nama" required>
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Tempat Lahir</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="ftempat_lahir" class="form-control" style="text-transform: capitalize;" placeholder="Tempat Lahir" required>                   
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Tanggal Lahir</label>
-                      <div class="col-sm-8">
-                        <input type="date" name="ftgl_lahir" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Jenis Kelamin</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fjenis_kelamin" class="form-control" style="text-transform: capitalize;" placeholder="Jenis Kelamin">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Agama</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fagama" class="form-control" style="text-transform: capitalize;" placeholder="Agama">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Jalan</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fjalan" class="form-control" style="text-transform: capitalize;" placeholder="Jalan">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Lingk / Dusun</label>
-                      <div class="col-sm-8">
-                      <input type="text" name="fdusun" class="form-control" style="text-transform: capitalize;" placeholder="Dusun">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">RT</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="frt" class="form-control" style="text-transform: capitalize;" placeholder="RT">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">RW</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="frw" class="form-control" style="text-transform: capitalize;" placeholder="RW">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Kel / Desa</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fdesa" class="form-control" style="text-transform: capitalize;" placeholder="Desa">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Kecamatan</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fkecamatan" class="form-control" style="text-transform: capitalize;" placeholder="Kecamatan">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Kota</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fkota" class="form-control" style="text-transform: capitalize;" placeholder="Kota">
-                      </div>
-                    </div>
+                    <?php
+                      $fields_left = [
+                        ["fnik", "NIK", "text", true],
+                        ["fnama", "Nama", "text", true],
+                        ["ftempat_lahir", "Tempat Lahir", "text", true],
+                        ["ftgl_lahir", "Tanggal Lahir", "date", false],
+                        ["fjenis_kelamin", "Jenis Kelamin", "text", false],
+                        ["fagama", "Agama", "text", false],
+                        ["fjalan", "Jalan", "text", false],
+                        ["fdusun", "Lingk / Dusun", "text", false],
+                        ["frt", "RT", "text", false],
+                        ["frw", "RW", "text", false],
+                        ["fdesa", "Kel / Desa", "text", false],
+                        ["fkecamatan", "Kecamatan", "text", false],
+                        ["fkota", "Kota", "text", false]
+                      ];
+                      foreach ($fields_left as $field) {
+                        echo '<div class="form-group">
+                          <label class="col-sm-4 control-label">' . $field[1] . '</label>
+                          <div class="col-sm-8">
+                            <input type="' . $field[2] . '" name="' . $field[0] . '" class="form-control" placeholder="' . $field[1] . '" ' . ($field[3] ? 'required' : '') . ' style="text-transform: capitalize;">
+                          </div>
+                        </div>';
+                      }
+                    ?>
                   </div>
                 </div>
+
+                <!-- Kolom Kanan -->
                 <div class="col-md-6">
                   <div class="box-body">
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Nomor KK</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fno_kk" maxlength="16" onkeypress="return hanyaAngka(event)" class="form-control" placeholder="Nomor KK">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Pendidikan di KK</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fpend_kk" class="form-control" style="text-transform: capitalize;" placeholder="Pendidikan di KK">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Pendidikan Terakhir</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fpend_terakhir" class="form-control" style="text-transform: capitalize;" placeholder="Pendidikan Terakhir">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Pendidikan Ditempuh</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fpend_ditempuh" class="form-control" style="text-transform: capitalize;" placeholder="Pendidikan Ditempuh">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Pekerjaan</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fpekerjaan" class="form-control" style="text-transform: capitalize;" placeholder="Pekerjaan">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Status Perkawinan</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fstatus_perkawinan" class="form-control" style="text-transform: capitalize;" placeholder="Status Perkawinan">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Status Dalam Keluarga</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fstatus_dlm_keluarga" class="form-control" style="text-transform: capitalize;" placeholder="Status Dalam Keluarga">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Kewarganegaraan</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fkewarganegaraan" class="form-control" style="text-transform: capitalize;" placeholder="Kewarganegaraan">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Nama Ayah</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fnama_ayah" class="form-control" style="text-transform: capitalize;" placeholder="Nama Ayah">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-4 control-label">Nama Ibu</label>
-                      <div class="col-sm-8">
-                        <input type="text" name="fnama_ibu" class="form-control" style="text-transform: capitalize;" placeholder="Nama Ibu">
-                      </div>
-                    </div>
+                    <?php
+                      $fields_right = [
+                        ["fno_kk", "Nomor KK", "text", false],
+                        ["fpend_kk", "Pendidikan di KK", "text", false],
+                        ["fpend_terakhir", "Pendidikan Terakhir", "text", false],
+                        ["fpend_ditempuh", "Pendidikan Ditempuh", "text", false],
+                        ["fpekerjaan", "Pekerjaan", "text", false],
+                        ["fstatus_perkawinan", "Status Perkawinan", "text", false],
+                        ["fstatus_dlm_keluarga", "Status Dalam Keluarga", "text", false],
+                        ["fkewarganegaraan", "Kewarganegaraan", "text", false],
+                        ["fnama_ayah", "Nama Ayah", "text", false],
+                        ["fnama_ibu", "Nama Ibu", "text", false]
+                      ];
+                      foreach ($fields_right as $field) {
+                        echo '<div class="form-group">
+                          <label class="col-sm-4 control-label">' . $field[1] . '</label>
+                          <div class="col-sm-8">
+                            <input type="' . $field[2] . '" name="' . $field[0] . '" class="form-control" placeholder="' . $field[1] . '" style="text-transform: capitalize;">
+                          </div>
+                        </div>';
+                      }
+                    ?>
                   </div>
-                  <div class="box-footer" style="text-align: right;">
+
+                  <div class="box-footer text-right">
                     <button type="button" class="btn btn-secondary" onclick="history.back()">Batal</button>
                     <button type="reset" class="btn btn-warning">Reset</button>
                     <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                   </div>
-
                 </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
-          <div class="box-footer">
-          </div>
+
+          <div class="box-footer"></div>
         </div>
       </div>
     </div>
   </section>
 </div>
+
+<script>
+  function hanyaAngka(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) return false;
+    return true;
+  }
+</script>
+
+
 
 <?php 
   include ('../part/footer.php');
