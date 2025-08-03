681433e8d38ffe1154d5484bc5c989290e6cae70
fardanjamhal <fardanjamhal@gmail.com>
2025-08-03 21:18:15 +0800

    perbaharui3

--- surat/part/header.php
+++ surat/part/header.php
@@ -12,7 +12,7 @@
 		?>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg py-1" style="background: linear-gradient(90deg, #0d47a1, #1976d2); box-shadow: 0 4px 12px rgba(0,174,255,0.4);">
 		<a class="navbar-brand d-flex align-items-center ml-3" href="../">
-			<img src="../assets/img/<?php echo $data['logo_desa']; ?>" alt="Logo" style="width: 45px; margin-right: 10px;">
+			<img src="../../assets/img/<?php echo $data['logo_desa']; ?>" alt="Logo" style="width: 45px; margin-right: 10px;">
 		</a>
 
 		<button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarContent">
