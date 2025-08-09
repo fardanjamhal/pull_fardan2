7855c4298aaf0da12475e61236423336931a746e
fardanjamhal <fardanjamhal@gmail.com>
2025-08-09 12:35:41 +0800

    perbaharui3

--- login/index.php
+++ login/index.php
@@ -159,7 +159,7 @@ html, body {
 @media (max-width: 768px) {
     .card {
         max-width: 400px; /* tetap sama */
-        min-height: 570px;
+        min-height: 540px;
     }
     .card-body form {
         max-width: 300px;
